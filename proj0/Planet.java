7a97504634bfdb39b5edc96356a10dfbb57724cd
fqzz2000 <fqzz2000@outlook.com>
2022-05-26 20:31:56 +0800

    finish calcForceExertedByX

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,6 +4,7 @@ public class Planet {
     public double xxVel;
     public double yyVel;
     public double mass;
+    public static double G = 6.67e-11;
     public String imgFileName;
     public Planet(Planet p){
         xxPos = p.xxPos;
@@ -23,5 +24,25 @@ public class Planet {
         imgFileName = img;
     }
 
+    public double calcDistance(Planet p){
+        double distSqr = (xxPos - p.xxPos)*(xxPos - p.xxPos) + (yyPos - p.yyPos)*(yyPos - p.yyPos);
+        return Math.sqrt(distSqr);
 
+    }
+
+    public double calcForceExertedBy(Planet p){
+        double r = calcDistance(p);
+        double force = G * mass * p.mass / (r * r);
+        return force;
+    }
+    public double calcForceExertedByX(Planet p){
+        double force = calcForceExertedBy(p);
+        double forceX = force * (p.xxPos - xxPos) / calcDistance(p);
+        return forceX;
+    }
+    public double calcForceExertedByY(Planet p){
+        double force = calcForceExertedBy(p);
+        double forceY = force * (p.yyPos - yyPos) / calcDistance(p);
+        return forceY;
+    }
 }
