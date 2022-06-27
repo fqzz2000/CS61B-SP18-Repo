1ff6e1b7878722d25a588b5cf6d07567acfc1c20
fqzz2000 <fqzz2000@outlook.com>
2022-06-27 11:35:10 +0800

    finish task 2

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,14 @@
 public class Palindrome {
+    /* convert a String to a Deque */
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
+    public boolean isPalindrome(String word) {
+        return false;
+    }
 }
